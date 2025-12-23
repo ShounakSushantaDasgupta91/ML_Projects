2d3a6895a5054bb0126910564c1a5964661bc91b
Shounak Sushanta Dasgupta <shounaksushantadasgupta@gmail.com>
2025-12-23 15:11:05 +0530

    Commit_V2

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name="ML_Project",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+    author="Shounak Sushanta Dasgupta",
+    author_email="shounaksushantadasgupta1991@gmail.com",
+    description="A simple data science package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+)
