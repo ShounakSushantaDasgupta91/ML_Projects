0e9c3076126855fc3090a7a54df19d19e818943f
Shounak Sushanta Dasgupta <shounaksushantadasgupta@gmail.com>
2025-12-23 23:26:09 +0530

    Commit_V4

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
@@ -17,20 +17,11 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name="ML_Project",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt"),
-    author="Shounak Sushanta Dasgupta",
-    author_email="shounaksushantadasgupta1991@gmail.com",
-    description="A simple data science package",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_package",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    python_requires='>=3.6'
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
